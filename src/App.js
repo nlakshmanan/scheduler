98d475ab6cd107dd615bcd8142aaa614cf30ab20
nlakshmanan <nnlnachu@gmail.com>
2020-01-16 12:04:19 -0600

    Slice 7

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import 'rbx/index.css';
-import { Button, Container, Title } from 'rbx';
+import { Message, Button, Container, Title } from 'rbx';
 import firebase from 'firebase/app';
 import 'firebase/database';
+import 'firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCBR_KbkMiiWuFsu1sC7cChP8FilfeLFnI",
@@ -14,6 +16,7 @@ const firebaseConfig = {
   appId: "1:784996429844:web:747b2779578818c723b701",
   measurementId: "G-WYP6GV9H5J"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database().ref();
@@ -78,8 +81,29 @@ const Course = ({course, state}) => (
   </Button>
 );
 
+const Welcome = ({ user }) => (
+  <Message color="info">
+    <Message.Header>
+      Welcome, {user.displayName}
+      <Button primary onClick={() => firebase.auth().signOut()}>
+        Log out
+      </Button>
+    </Message.Header>
+  </Message>
+);
+
+const SignIn = () => (
+  <StyledFirebaseAuth
+    uiConfig={uiConfig}
+    firebaseAuth={firebase.auth()}
+  />
+);
+
 const Banner = props => (
+  <React.Fragment>
+    { props.user ? <Welcome user={ props.user } /> : <SignIn /> }
   <Title> {props.title || '[loading..]'} </Title> 
+  </React.Fragment>
 );
 
 const buttonColor = selected => (
@@ -145,12 +169,23 @@ const useSelection = () => {
   return [selected, toggle];
 }
 
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
+
 
 
 
 const App = () => {
   const [schedule, setSchedule] = useState({title:'',courses:[]});
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  const [user, setUser] = useState(null);
   
   useEffect(() => {
     const handleData = snap => {
@@ -160,9 +195,13 @@ const App = () => {
     return () => { db.off('value',handleData); };
   },[]);
 
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, []);
+
   return (
    <Container>
-     <Banner title = { schedule.title}/>
+     <Banner title = { schedule.title} user = {user}/>
      <CourseList courses = { schedule.courses} />
    </Container>
   )
